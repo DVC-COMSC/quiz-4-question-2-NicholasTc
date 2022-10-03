6452748fbfe0f9b48d07a42ccf55501de13ee2f2
Nicholas <nicholastc14@gmail.com>
2022-10-02 22:39:01 -0700

    Quiz 1 question 2

--- q2.py
+++ q2.py
@@ -2,8 +2,32 @@
 # ******************************
 # Make your Code
 # ******************************
+longest = ""
+shortest = ""
 
+counter = 0
+while True:
+    input_string = input()
 
+    if input_string == "stop":
+        break
+    
+    if counter == 0:
+        longest = input_string
+        shortest = input_string
+
+    # checks the length
+    input_len = len(input_string)
+
+    if input_len > len(longest):
+        longest = input_string
+
+    if input_len < len(shortest):
+        shortest = input_string
+
+    counter += 1
+
+print(f"{longest} {shortest}")
 # Requirement
 # No need to use list
 # All input values are taken one by one separatively.
